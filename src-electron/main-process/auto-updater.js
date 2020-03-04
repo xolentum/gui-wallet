c3bc2477374a293e4d98cdc0495b4f6a2871b77c
Mikunj <mikunj@live.com.au>
2020-03-04 15:32:46 +1100

    Refactor auto updating

--- src-electron/main-process/auto-updater.js
+++ src-electron/main-process/auto-updater.js
@@ -1,89 +1,91 @@
 import { dialog } from "electron";
 import isDev from "electron-is-dev";
 import { autoUpdater } from "electron-updater";
-import ProgressBar from "electron-progressbar";
 
-let progressBar = null;
 let isUpdating = false;
-let downloadAndInstall = false;
 
-function checkForUpdate(onQuitAndInstall) {
+async function checkForUpdate(getMainWindow, onQuitAndInstall) {
   // Disable for development
   if (isDev) {
     return;
   }
 
+  if (isUpdating) {
+    return;
+  }
+
   autoUpdater.logger = console;
-  autoUpdater.autoDownload = false;
 
-  autoUpdater.on("error", err => {
-    if (isUpdating) {
-      dialog.showErrorBox("Update Error: ", err == null ? "unknown" : err.message);
-      isUpdating = false;
-      console.error("Error in auto-updater.", err.message);
-    }
-  });
+  try {
+    // Get the update using electron-updater
+    try {
+      const info = await autoUpdater.checkForUpdates();
+      if (!info.downloadPromise) {
+        console.info("auto-update: no update to download");
 
-  autoUpdater.on("update-available", info => {
-    console.log(`Update available: ${info.version}`);
-
-    const message = `Update ${info.version} found. Do you want to download the update?`;
-    const detail = `View the release notes at: https://github.com/loki-project/loki-electron-gui-wallet/releases/tag/v${info.version}`;
-
-    dialog.showMessageBox(
-      {
-        type: "info",
-        title: "Update available",
-        message,
-        detail,
-        buttons: ["Download and Install", "Download and Install Later", "No"],
-        defaultId: 0
-      },
-      buttonIndex => {
-        // Download and install
-        if (buttonIndex === 0) {
-          downloadAndInstall = true;
-          if (!progressBar) {
-            progressBar = new ProgressBar({
-              indeterminate: false,
-              title: "Downloading...",
-              text: `Downloading wallet v${info.version}`
-            });
-          }
-        }
-
-        // Download
-        if (buttonIndex !== 2) {
-          isUpdating = true;
-          autoUpdater.downloadUpdate();
-        }
+        return;
       }
-    );
-  });
-
-  autoUpdater.on("download-progress", progress => {
-    if (progressBar) {
-      progressBar.value = progress.percent;
-    }
-  });
-
-  autoUpdater.on("update-downloaded", () => {
-    console.log("Update downloaded");
-    isUpdating = false;
-
-    if (progressBar) {
-      progressBar.setCompleted();
-      progressBar = null;
+      await info.downloadPromise;
+    } catch (error) {
+      await showCannotUpdateDialog(getMainWindow());
+      throw error;
     }
 
-    // If download and install was selected then quit and install
-    if (downloadAndInstall && onQuitAndInstall) {
+    // Update downloaded successfully, we should ask the user to update
+    console.info("auto-update: showing update dialog...");
+    const shouldUpdate = await showUpdateDialog(getMainWindow());
+    if (!shouldUpdate) {
+      return;
+    }
+
+    console.info("auto-update: calling quitAndInstall...");
+    if (onQuitAndInstall) {
       onQuitAndInstall(autoUpdater);
-      downloadAndInstall = false;
     }
-  });
+  } catch (error) {
+    console.error("auto-update error:", getPrintableError(error));
+  } finally {
+    isUpdating = false;
+  }
+}
 
-  autoUpdater.checkForUpdates();
+function getPrintableError(error) {
+  return error && error.stack ? error.stack : error;
+}
+
+async function showUpdateDialog(mainWindow) {
+  const RESTART_BUTTON = 0;
+  const LATER_BUTTON = 1;
+  const options = {
+    type: "info",
+    buttons: ["Restart Wallet", "Later"],
+    title: "Loki Electron Wallet update available",
+    message: "There is a new version of Loki Electron Wallet available.",
+    detail: "Press Restart Wallet to apply the update",
+    defaultId: LATER_BUTTON,
+    cancelId: RESTART_BUTTON
+  };
+  return new Promise(resolve => {
+    dialog.showMessageBox(mainWindow, options, response => {
+      resolve(response === RESTART_BUTTON);
+    });
+  });
+}
+
+async function showCannotUpdateDialog(mainWindow) {
+  const options = {
+    type: "error",
+    buttons: ["Ok"],
+    title: "Cannot update",
+    message:
+      "Loki Electron Wallet failed to update but there is a new version available. Please go to https://loki.network/ and install the new version manually."
+  };
+
+  return new Promise(resolve => {
+    dialog.showMessageBox(mainWindow, options, () => {
+      resolve();
+    });
+  });
 }
 
 export { checkForUpdate };
