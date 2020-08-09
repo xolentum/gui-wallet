8900a6e90dafb02a4e6e32457d9ab7d8eb5dcfa0
Sayan Bhattacharyya <sohamb03@outlook.com>
2020-08-09 12:09:27 +0530

    Update links

--- build/download-binaries.js
+++ build/download-binaries.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 async function download() {
   const { platform, env } = process;
-  const repoUrl = "https://api.github.com/repos/loki-project/loki/releases/latest";
+  const repoUrl = "https://api.github.com/repos/xolentum/xolentum/releases/latest";
   try {
     const pwd = process.cwd();
     const downloadDir = path.join(pwd, "downloads");
@@ -12,7 +12,7 @@ async function download() {
 
     const headers = {
       "Content-Type": "application/json",
-      "User-Agent": "Loki-Electron-Wallet"
+      "User-Agent": "Xolentum-Electron-Wallet"
     };
     if (env.GH_TOKEN) {
       headers.Authorisation = `Bearer ${env.GH_TOKEN}`;
